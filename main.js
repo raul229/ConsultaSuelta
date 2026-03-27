76f925cb5fab0a8af7e940d0571e67ffcc645e4e
raul <raulzambranoleon15@gmail.com>
2026-03-26 22:01:34 -0500

    feat: avisar que necesita token

--- main.js
+++ main.js
@@ -1,49 +1,52 @@
-const {Client, LocalAuth} = require("whatsapp-web.js");
+const { Client, LocalAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
 require("dotenv").config();
 const axios = require("axios");
 
 const cliente = new Client({
-    authStrategy: new LocalAuth({
-        clientId: "ConsultaSuelta"
-    }),
-    puppeteer: {headless: true}
+  authStrategy: new LocalAuth({
+    clientId: "ConsultaSuelta",
+  }),
+  puppeteer: { headless: true },
 });
 
-cliente.on("qr", qr=>{
-    qrcode.generate(qr, {small: true});
+cliente.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
 });
 
-cliente.on("ready", async ()=>{
-    console.log("Bienvenido");
-    // const chats = await cliente.getChats();
-    // chats.forEach(chat => {
-    //     if(chat.isGroup){
-    //         console.log(chat.name, chat.id._serialized)
-    //     }
-    // })
+cliente.on("ready", async () => {
+  console.log("Bienvenido");
+  //   const chats = await cliente.getChats();
+  //   chats.forEach((chat) => {
+  //     if (chat) {
+  //       console.log(chat.name, chat.id._serialized);
+  //     }
+  //   });
+
+  //   console.log("mi id", cliente.info.me._serialized);
 });
 
-cliente.on("message", msg=>{
-    if(msg.from!== process.env.PRUEBA) return;
-    procesarMsg(msg);
-})
+cliente.on("message", (msg) => {
+  if (msg.from == process.env.PRUEBA || msg.from == process.env.PERSONAL) {
+    procesarMensaje(msg);
+  }
+});
 
-async function procesarMsg(msg) {
-    try{
-        
-        const res = await axios.post("http://0.0.0.0:8000/consulasuelta/", {
-            ruc: msg.body
-        });
-       
-        await msg.reply(JSON.stringify(res.data));
+async function procesarMensaje(msg) {
+  try {
+    console.log(msg.body);
 
-       
+    const res = await axios.post("http://0.0.0.0:8000/consulasuelta/", {
+      ruc: msg.body,
+    });
+    if (Object.keys(res.data).length === 0) {
+      await cliente.sendMessage(process.env.PERSONAL, "ACTUALIZA EL TOKEN");
+    } else {
+      await msg.reply(JSON.stringify(res.data));
     }
-    catch(error){
-        console.log(error);
-    }
-    
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 cliente.initialize();
